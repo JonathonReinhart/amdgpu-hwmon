d339cf3df4a3618320447da0f98c7dd3f9b9ecf4
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2018-12-17 22:35:48 -0500

    Add options to set mode/level

--- amdgpu.py
+++ amdgpu.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # https://github.com/torvalds/linux/blob/v4.19/drivers/gpu/drm/amd/amdgpu/amdgpu_pm.c#L1315
+import argparse
 from sysfs import SysFsObject
+
+def invert_map(m):
+    return {v: k for k, v in m.items()}
 
 class AmdGpuHwmon:
     def __init__(self, cardnum=0):
@@ -28,23 +32,52 @@ class AmdGpuHwmon:
     def fan_pwm_mode(self, value):
         self.hwmon.pwm1_enable = value
 
+def pwm_level(x):
+    lvl = float(x)
+    if lvl < 0 or lvl > 1:
+        raise ValueError("Level must be between 0.0 and 1.0")
+    return lvl
 
+mode_map = {
+    'none': 0,
+    'manual': 1,
+    'auto': 2,
+}
+
+def parse_args():
+    ap = argparse.ArgumentParser()
+
+    grp = ap.add_mutually_exclusive_group()
+    grp.add_argument('-l', '--level', metavar='LEVEL', type=pwm_level,
+            help="Manually set fan speed to LEVEL [0.0 - 1.0]")
+    grp.add_argument('-a', '--auto', action='store_true',
+            help="Enable automatic fan speed control")
+
+    args = ap.parse_args()
+    return args
 
 def main():
+    args = parse_args()
+
     a = AmdGpuHwmon()
+
+    # Set
+    if args.level:
+        assert not args.auto
+        a.fan_pwm_mode = mode_map['manual']
+        a.fan_pwm = args.level
+
+    if args.auto:
+        assert args.level is None
+        a.fan_pwm_mode = mode_map['auto']
+
+
+    # Show
     print("Temperature: {} C".format(a.temperature))
 
     mode = a.fan_pwm_mode
-    print("Fan mode: {} ({})".format(mode, {
-            0: "no fan speed control",
-            1: "manual fan speed control using pwm interface",
-            2: "automatic fan speed control",
-        }.get(mode, "???")))
+    print("Fan mode: {} ({})".format(mode, invert_map(mode_map).get(mode, '???')))
     print("Fan PWM: {:.02f} %".format(a.fan_pwm * 100))
-
-
-    a.fan_pwm_mode = 1
-    a.fan_pwm = 1.0
 
 
 if __name__ == '__main__':
